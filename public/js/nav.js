7c3c553b745cd8500401f33bc5cecb823ecf723a
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-01-17 21:19:43 +0900

    user img

--- public/js/nav.js
+++ public/js/nav.js
@@ -9,7 +9,13 @@ const createNav = () => {
                          <input type="text" class="search-box" placeholder="검색">
                          <button class="search-btn">검색</button>
                     </div>
-                    <a href="#"><img src="img/user.png" alt=""></a>
+                    <a>
+                         <img src="img/user.png" id="user-img" alt="">
+                         <div class="login-logout-popup hide">
+                              <p class="account-info">다음으로 로그인, 이름</p>
+                              <button class="btn" id="user-btn">로그아웃</button>
+                         </div>
+                    </a>
                     <a href="#"><img src="img/cart.png" alt=""></a>
                </div>
           </div>
@@ -24,3 +30,33 @@ const createNav = () => {
 }
 
 createNav();
+
+// nav popup
+const userImageButton = document.querySelector('#user-img');
+const userPop = document.querySelector('.login-logout-popup');
+const popupText = document.querySelector('.account-info');
+const actionBtn = document.querySelector('#user-btn');
+
+userImageButton.addEventListener('click', () => {
+     userPop.classList.toggle('hide');
+})
+
+window.onload = () => {
+     let user = JSON.parse(sessionStorage.user || null);
+     if (user != null) {
+          // means user is logged in
+          popupText.innerHTML = `다음으로 로그인, ${user.name}`;
+          actionBtn.innerHTML = '로그아웃';
+          actionBtn.addEventListener('click', () => {
+               sessionStorage.clear();
+               location.reload();
+          })
+     } else {
+          // user is logged out
+          popupText.innerHTML = '로그인 후 주문하기';
+          actionBtn.innerHTML = '로그인'
+          actionBtn.addEventListener('click', () => {
+               location.href = '/login'
+          })
+     }
+}
