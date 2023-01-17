63c69fa555abfe393d9f8c14ead4a5be408b14c4
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-01-17 20:31:33 +0900

    login

--- public/js/form.js
+++ public/js/form.js
@@ -12,72 +12,50 @@ const loader = document.querySelector('.loader');
 
 // select inputs
 const submitBtn = document.querySelector('.submit-btn');
-const name = document.querySelector('#name');
+const name = document.querySelector('#name') || null;
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const number = document.querySelector('#number');
-const tac = document.querySelector('#terms-and-cond');
-const notification = document.querySelector('#notification');
+const number = document.querySelector('#number') || null;
+const tac = document.querySelector('#terms-and-cond') || null;
+const notification = document.querySelector('#notification') || null;
 
 submitBtn.addEventListener('click', () => {
-     if (name.value.length < 3) {
-          showAlert('이름은 3글자 이상 필수입니다.');
-     } else if (!email.value.length) {
-          showAlert('이메일 입력은 필수입니다.')
-     } else if (password.value.length < 8) {
-          showAlert('비밀번호는 8글자 이상 입력해야 합니다.')
-     } else if (!number.value.length) {
-          showAlert('휴대폰 번호를 입력해주세요.')
-     } else if (!Number(number.value) || number.value.length < 10) {
-          showAlert('잘못된 번호입니다. 유효한 번호를 입력하세요.')
-     } else if (!tac.checked) {
-          showAlert('이용 약관에 동의해야 합니다.');
+     if (name != null) { // sign up page
+          if (name.value.length < 3) {
+               showAlert('이름은 3글자 이상 필수입니다.');
+          } else if (!email.value.length) {
+               showAlert('이메일 입력은 필수입니다.')
+          } else if (password.value.length < 8) {
+               showAlert('비밀번호는 8글자 이상 입력해야 합니다.')
+          } else if (!number.value.length) {
+               showAlert('휴대폰 번호를 입력해주세요.')
+          } else if (!Number(number.value) || number.value.length < 10) {
+               showAlert('잘못된 번호입니다. 유효한 번호를 입력하세요.')
+          } else if (!tac.checked) {
+               showAlert('이용 약관에 동의해야 합니다.');
+          } else {
+               // submit form
+               loader.style.display = 'block';
+               sendData('/signup', {
+                    name: name.value,
+                    email: email.value,
+                    password: password.value,
+                    number: number.value,
+                    tac: tac.checked,
+                    notification: notification.checked,
+                    seller: false
+               })
+          }
      } else {
-          // submit form
-          loader.style.display = 'block';
-          sendData('/signup', {
-               name: name.value,
-               email: email.value,
-               password: password.value,
-               number: number.value,
-               tac: tac.checked,
-               notification: notification.checked,
-               seller: false
-          })
+          // login page
+          if (!email.value.length || !password.value.length) {
+               showAlert('이메일과 비밀번호 모두를 입력해야 합니다.')
+          } else {
+               loader.style.display = 'block';
+               sendData('/login', {
+                    email: email.value,
+                    password: password.value,
+               })
+          }
      }
 })
-
-// send data function
-const sendData = (path, data) => {
-     fetch(path, {
-          method: 'post',
-          headers: new Headers({ 'Content-Type': 'application/json' }),
-          body: JSON.stringify(data)
-     }).then((res) => res.json())
-     .then(response => {
-          processData(response)     
-     })
-}
-
-const processData = (data) => {
-     loader.style.display = null;
-     if (data.alert) {
-          showAlert(data.alert);
-     } else if (data.name) {
-          // create authToken
-          data.authToken = generateToken(data.email);
-          sessionStorage.user = JSON.stringify(data);
-          location.replace('/');
-     }
-}
-
-// alert function
-const showAlert = (msg) => {
-     let alertBox = document.querySelector('.alert-box');
-     let alertMsg = document.querySelector('.alert-msg');
-     alertMsg.innerHTML = msg;
-     alertBox.classList.add('show');
-     setTimeout(() => {
-          alertBox.classList.remove('show');
-     }, 3000);
-}
--- server.js
+++ server.js
@@ -77,6 +77,39 @@ app.post('/signup', (req, res) => {
      })
 })
 
+// login route
+app.get('/login', (req, res) => {
+     res.sendFile(path.join(staticPath, "login.html"))
+})
+
+app.post('/login', (req, res) => {
+     let { email, password } = req.body;
+
+     if (!email.length || !password.length) {
+          return res.json({'alert': '이메일과 비밀번호를 모두 입력하시오'})
+     }
+
+     db.collection('users').doc(email).get()
+     .then(user => {
+          if (!user.exists) {
+               return res.json({'alert': '이메일이 존재하지 않습니다.'})
+          } else {
+               bcrypt.compare(password, user.data().password, (err, result) => {
+                    if (result) {
+                         let data = user.data();
+                         return res.json({
+                              name: data.name,
+                              email: data.email,
+                              seller: data.seller,
+                         })
+                    } else {
+                         return res.json({'alert': '비밀번호가 올바르지 않습니다.'})
+                    }
+               })
+          }
+     })
+})
+
 // 404 route
 app.get('/404', (req, res) => {
      res.sendFile(path.join(staticPath, "404.html"));
